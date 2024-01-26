c23303f315ceeea488f5ec6e00cd61c2ed8d7117
BoTlInYQ <130334665+BoTlInYQ@users.noreply.github.com>
2024-01-26 14:03:49 -0800

    Add files via upload

--- sshell.c
+++ sshell.c
@@ -11,27 +11,27 @@
 #define CMDARGU_MAX 16 //A program has a maximum of 16 non-null arguments.
 #define TOKEN_MAX 32 //The maximum length of individual tokens never exceeds 32 characters.
 
-/*Command properties*/
-struct Command{
+//Command properties
+typedef struct{
         char cmd[CMDLINE_MAX];
         char *args[CMDARGU_MAX + 1]; // +1 for the NULL pointer
         char *outputFile;
-};
+}Command;
 
 void print_complete_message(char *cmd, int status){
         fprintf(stderr, "+ completed '%s' [%d]\n", cmd, status);
 }
 
-/*check for redirection signal '>'*/
+//check for redirection signal '>'
 int redirection(char* cmd){
-        if(strchr(cmd,'>') != NULL && strstr(cmd,">>") == NULL){
+        if(strchr(cmd,'>') != NULL){
                 return 1;
         }else{
                 return 0;
         }
 }
 
-/*check for append signal '>>'*/
+//check for append signal '>>'
 int append(char* cmd){
         if(strstr(cmd,">>") != NULL){
                 return 1;
@@ -40,7 +40,7 @@ int append(char* cmd){
         }
 }
 
-/*check for pipe signal '|'*/
+//check for pipe signal '|'
 int pipeline(char* cmd){
         if(strchr(cmd,'|') != NULL){
                 return 1;
@@ -50,128 +50,161 @@ int pipeline(char* cmd){
 }
 
 // Function to initialize a Command instance
-int initCommand(struct Command *command, const char *cmdLine) {
-        char *token;
+int initCommand(Command *command, char *cmdLine) {
+        char commandline_copy[CMDLINE_MAX];
         char *spaceDelimiter = " ";
+        char *cmd;
         int arg_count = 0;
-
-        strncpy(command->cmd, cmdLine, sizeof(command->cmd));
-        command->cmd[sizeof(command->cmd) - 1] = '\0';
-
-        // Initialize args array
-        token = strtok(command->cmd, spaceDelimiter);
-        while (token != NULL && arg_count < CMDARGU_MAX) {
-                if (strlen(token) < TOKEN_MAX){
-                        command->args[arg_count++] = token;
-                        token = strtok(NULL, spaceDelimiter);
-                }else{
-                        fprintf(stderr, "Error: reach token maximum\n");
+        
+         // Make a copy of cmdline to avoid modifying the original
+        strcpy(commandline_copy, cmdLine);
+        
+        cmd = strtok(commandline_copy, spaceDelimiter);
+        if (cmd != NULL) {
+                strcpy(command->cmd, cmd);
+                }
+        
+        while (cmd != NULL) {
+                // check if too many argument provided
+                if (arg_count >= CMDARGU_MAX) { 
+                        fprintf(stderr, "Error: too many process arguments\n");
                         return 1;
                 }
-                if(arg_count >= CMDARGU_MAX){
-                        fprintf(stderr,"Error: too many process arguments\n");
+
+                command->args[arg_count] = strdup(cmd);
+
+                //check memory allocation errors
+                if (command->args[arg_count] == NULL) {
+                        perror("Error: strdup failed");
                         return 1;
                 }
+                cmd = strtok(NULL, spaceDelimiter);
+                arg_count++;
         }
-
-        // Set remain args to NULL
-        while(arg_count < CMDARGU_MAX){
-                command->args[arg_count] = NULL;
-                arg_count++; 
-        }
+        // Set the last element of command->args to NULL
+        command->args[arg_count] = NULL;
         return 0;
 }
 
 // Function to initialize a Command with redirection.
-int initCommand_redirection(struct Command *command, const char *cmdLine){
-        char *token;
+int initCommand_redirection(Command *command, const char *cmdLine){
+        char commandline_copy[CMDLINE_MAX];
+        char *cmd;
+        char *cmd_before_redir;
+        char *cmd_after_redir;
+        char *file;
         char *spaceDelimiter = " ";
+        char *redirectionDelimiter = ">";
         int arg_count = 0;
 
-        strncpy(command->cmd, cmdLine, sizeof(command->cmd));
-        command->cmd[sizeof(command->cmd) - 1] = '\0';
-
-        // Initialize args array
-        token = strtok(command->cmd, spaceDelimiter);
-
-        /*check if the first character is redirection character and print error message if Yes*/
-        if (token[0] ==  '>') {
+        // Make a copy of cmdline to avoid modifying the original
+        strcpy(commandline_copy, cmdLine);
+        //check if the first character is redirection character and print error message if Yes
+        if (commandline_copy[0] ==  '>') {
 		fprintf(stderr, "Error: missing command\n");
 		return 1;
 	}
 
+        strcpy(commandline_copy, cmdLine);
+        
+        //separate commandline into two parts.
+        cmd_before_redir =strtok(commandline_copy, redirectionDelimiter);
+        cmd_after_redir = strtok(NULL, redirectionDelimiter);
+        //ignore the lending space behind the redirection delimiter.
+        cmd_after_redir = strtok(cmd_after_redir, spaceDelimiter);
 
-        while (token != NULL && arg_count < CMDARGU_MAX) {
-                if (strlen(token) < TOKEN_MAX){
-                        command->args[arg_count++] = token;
-                        token = strtok(NULL, spaceDelimiter);
-                }else{
-                        fprintf(stderr, "Error: reach token maximum\n");
-                        return 1;
-                }
-                if(arg_count >= CMDARGU_MAX){
-                        fprintf(stderr,"Error: too many process arguments\n");
-                        return 1;
-                }
+        if(cmd_after_redir == NULL){
+                command->outputFile = NULL; 
+        }else{
+                command->outputFile = strdup(cmd_after_redir);
         }
 
-        // Set remain args to NULL
-        while(arg_count < CMDARGU_MAX){
-                command->args[arg_count] = NULL;
-                arg_count++; 
+        cmd = strtok(cmd_before_redir, spaceDelimiter);
+        if (cmd != NULL) {
+                strcpy(command->cmd, cmd);
         }
+
+        while (cmd != NULL) {
+                // check if too many argument provided
+                if (arg_count >= CMDARGU_MAX) { 
+                        fprintf(stderr, "Error: too many process arguments\n");
+                        return 1;
+                }
+                command->args[arg_count] = strdup(cmd);
+                //check memory allocation errors
+                if (command->args[arg_count] == NULL) {
+                        perror("Error: strdup failed");
+                        return 1;
+                }
+                cmd = strtok(NULL, spaceDelimiter);
+                arg_count++;
+        }
+        // Set the last element of command->args to NULL
+        command->args[arg_count] = NULL;
         return 0;
 }
 
 // Function to initialize a Command with pipeline.
-int initCommand_pipeline(struct Command *command, const char *cmdLine){
-
+int initCommand_pipeline(Command *command, const char *cmdLine){
+        return 1;
 }
 
 // Function to initialize a Command with appends.
-int initCommand_append(struct Command *command, const char *cmdLine){
-
+int initCommand_append(Command *command, const char *cmdLine){
+        return 1;
 }
 
 // Function to execute a Command instance
-void executeCommand(struct Command *command) {
+int executeCommand(Command *command, int is_redirection, int is_append) {
         pid_t pid;
+        int fd;
         int status;
 
         pid = fork();
         if (pid == 0) {
                 // Child process
-                execvp(command->args[0], command->args);
+                if(is_redirection){
+                        if(command->outputFile == NULL){
+                                fprintf(stderr, "Error: no output file\n");
+				exit(1); 
+                        }
+                        if(is_append){
+                                fd = open(command->outputFile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+                        }else{
+                                fd = open(command->outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                        }
+                        if (fd == -1) {
+                                fprintf(stderr, "Error: cannot open output file\n");
+				exit(1);
+			}
+                        dup2(fd, STDOUT_FILENO);
+                        close(fd);
+                }
+                execvp(command->cmd, command->args);
                 perror("execvp");
                 exit(EXIT_FAILURE);
         } else if (pid > 0) {
                 // Parent process
                 wait(&status);
                 if (WIFEXITED(status)) {
-                        print_complete_message(command->cmd, status);
+                        return 0;
                 }
         } else {
                 // Fork failed
                 perror("fork");
                 exit(EXIT_FAILURE);
         }
+        return 1;
 }
 
 // Function to process pwd command.
-int handle_pwd(){
-        char cwd[CMDLINE_MAX];
-
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-                printf("%s\n", cwd);
-                return 0;
-        } else {
-                perror("getcwd");
-                return 1;
-        }
+int handle_pwd(char* directory){
+        fprintf(stdout, "%s\n", directory);
+        return 0;
 }
 
 // Function to process cd command.
-int handle_cd(const struct Command *command){
+int handle_cd(Command *command){
         DIR * dir;
 
         if (command->args[1] != NULL) {
@@ -194,10 +227,11 @@ int handle_exit(){
         return 0;
 }
 
+// Function to process sls command.
 int handle_sls(){
         DIR *dirp;
         struct dirent *dp;
-        struct stat file_stat;
+        struct stat file;
         
         dirp = opendir(".");
         if (dirp == NULL) {
@@ -206,18 +240,18 @@ int handle_sls(){
         }
         
         while ((dp = readdir(dirp)) != NULL) {
-                // Skip "." and ".." entries
+                //Skip "." and ".." entries
                 if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
                         continue;
                 }
                 // Obtain file information
-                if (stat(dp->d_name, &file_stat) == -1) {
+                if (stat(dp->d_name, &file) == -1) {
                         perror("stat");
                         closedir(dirp);
                         return 1;
                 }
-                // Print entry and its size
-                printf("%s (%ld bytes)\n", dp->d_name, (long)file_stat.st_size);
+                //Print entry and its size
+                printf("%s (%ld bytes)\n", dp->d_name, (long)file.st_size);
         }
         closedir(dirp);
         return 0;
@@ -227,11 +261,21 @@ int handle_sls(){
 int main(void)
 {
         char cmd[CMDLINE_MAX];
+        char directory[CMDLINE_MAX];
         char *nl;
         int status;
-        struct Command myCommand;
+        int is_redirection;
+        int is_pipe;
+        int is_append;
 
         while (1) {
+                //Initialize flag and structure.
+                is_redirection = 0;
+                is_pipe = 0;
+                is_append = 0;
+                Command *myCommand = malloc(sizeof(Command));
+                char *cmdCopy;
+                
                 /* Print prompt */
                 printf("sshell@ucd$ ");
                 fflush(stdout);
@@ -249,42 +293,63 @@ int main(void)
                 nl = strchr(cmd, '\n');
                 if (nl)
                         *nl = '\0';
+                
+                //Parsing command line
+                cmdCopy = strdup(cmd);
+                if(redirection(cmdCopy)){
+                        is_redirection = 1;
+                }
+
+                cmdCopy = strdup(cmd);
+                if(pipeline(cmdCopy)){
+                        is_pipe = 1;
+                }
+
+                cmdCopy = strdup(cmd);
+                if(append(cmdCopy)){
+                        is_append = 1;
+                }
 
                 // Initialize Command instance
-                if(redirection(cmd)){
-                        status = initCommand_redirection(&myCommand, cmd);
+                if(is_redirection && !is_pipe){
+                        status = initCommand_redirection(myCommand, cmd);
                         if(status)
                                 continue;
-                }else if(pipeline(cmd)){
-                        status = initCommand_pipeline(&myCommand, cmd);
+                }else if(is_pipe){
+                        status = initCommand_pipeline(myCommand, cmd);
                         if(status)
                                 continue;
-                }else if(append(cmd)){
-                        status = initCommand_append(&myCommand, cmd);
-                        if(status)
-                                continue;
-                }else{
-                        status = initCommand(&myCommand, cmd);
+                }else if(!is_redirection && !is_pipe){
+                        status = initCommand(myCommand, cmd);
                         if(status)
                                 continue;
                 }
+
                 /* Handle built-in command "exit" */
-                if (!strcmp(myCommand.args[0], "exit")) {
+                if (!strcmp(myCommand->args[0], "exit")) {
                         status = handle_exit();
                         print_complete_message(cmd, status);
                         break;
-                }else if(!strcmp(myCommand.args[0], "cd")){ /* Handle built-in command "cd" */
-                        status = handle_cd(&myCommand);
+                }else if(!strcmp(myCommand->args[0], "cd")){ // Handle built-in command "cd" 
+                        status = handle_cd(myCommand);
                         print_complete_message(cmd, status);
-                }else if(!strcmp(myCommand.args[0], "pwd")){/* Handle built-in command "pwd" */
-                        status = handle_pwd();
+                }else if(!strcmp(myCommand->args[0], "pwd")){// Handle built-in command "pwd" 
+                        getcwd(directory, sizeof(directory));
+                        status = handle_pwd(directory);
                         print_complete_message(cmd, status);
-                }else if(!strcmp(myCommand.args[0], "sls")){
+                }else if(!strcmp(myCommand->args[0], "sls")){
                         status = handle_sls();
                         print_complete_message(cmd, status);
                 }else{
-                        executeCommand(&myCommand);// Execute the Command
+                        status = executeCommand(myCommand, is_redirection, is_append);// Execute the Command                              
+                        if (status){
+                                continue;
+                        }
+                        print_complete_message(cmd, status);        
                 }
+
+                free(cmdCopy);
+                free(myCommand);
         }
 
         return EXIT_SUCCESS;
